bf16b550442a366d24a802cc6653f298287ff0da
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2023-08-20 00:24:57 -1000

    Make it Astro, make it dope 🚀

---
+++ src/components/Nav.svelte
@@ -0,0 +1,49 @@
+<nav>
+  <div class="left">
+    <a href="#top">
+      <h1>DECKER.</h1>
+    </a>
+  </div>
+  <div class="right">
+    <ul>
+      <a href="#projects">
+        <li>Projects</li>
+      </a>
+      <a href="#experience">
+        <li>Experience</li>
+      </a>
+      <a href="#contact">
+        <li>Contact</li>
+      </a>
+    </ul>
+  </div>
+</nav>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap");
+
+  nav {
+    align-items: center;
+    border-bottom: 1px solid var(--color-border);
+    color: var(--color-text);
+    display: flex;
+    justify-content: space-between;
+    padding: 0 2rem;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  a:hover {
+    color: var(--color-accent);
+  }
+  h1 {
+    font-family: Roboto Mono, monospace;
+    text-transform: uppercase;
+  }
+
+  ul {
+    display: flex;
+    gap: 1.5rem;
+  }
+</style>

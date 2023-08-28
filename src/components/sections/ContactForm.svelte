7ed87ab9186d8ac046d9c7f9095e948a77d1b0c6
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2023-08-27 21:26:48 -1000

    Style the contact form 🎨

--- src/components/sections/ContactForm.svelte
+++ src/components/sections/ContactForm.svelte
@@ -1,33 +1,55 @@
-<script lang="ts">
-  let responseMessage: string;
-
-  async function submit(e: SubmitEvent) {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget as HTMLFormElement);
-    const response = await fetch("/api/feedback", {
-      method: "POST",
-      body: formData,
-    });
-    const data = await response.json();
-    responseMessage = data.message;
-  }
-</script>
-
-<form on:submit={submit}>
-  <label>
-    Name
-    <input type="text" id="name" name="name" required />
-  </label>
-  <label>
-    Email
-    <input type="email" id="email" name="email" required />
-  </label>
-  <label>
-    Message
-    <textarea id="message" name="message" required />
-  </label>
+<form method="post" action="https://formsubmit.co/el/wobowe">
+  <label for="name"> Name </label>
+  <input type="text" id="name" name="name" required />
+  <label for="email"> Email </label>
+  <input type="email" id="email" name="email" required />
+  <label for="message"> Message </label>
+  <textarea id="message" name="message" required />
   <button>Send</button>
-  {#if responseMessage}
-    <p>{responseMessage}</p>
-  {/if}
 </form>
+
+<style>
+  form {
+    display: grid;
+    border-radius: 8px;
+    background: var(--color-bg-light);
+    gap: 1rem;
+    padding: 2rem;
+  }
+
+  :is(label, input, textarea, button) {
+    appearance: none;
+    background: inherit;
+    border: none;
+    color: var(--color-text-dark);
+    font-family: var(--font-family);
+    outline: none;
+  }
+
+  :is(input, textarea) {
+    background-color: var(--color-text);
+    border: 1px solid var(--color-text-dark);
+    border-radius: 4px;
+    padding: 0.5rem;
+  }
+
+  :is(input, textarea):hover,
+  :is(input, textarea):focus {
+    outline: 1px solid var(--color-text-dark);
+    outline-offset: 0px;
+  }
+
+  button {
+    background: var(--color-bg);
+    border-radius: 4px;
+    color: var(--color-text-light);
+    padding: 1rem 2rem;
+    width: fit-content;
+  }
+
+  button:hover,
+  button:focus {
+    cursor: pointer;
+    outline: 2px solid green;
+  }
+</style>

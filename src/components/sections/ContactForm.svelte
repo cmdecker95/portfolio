bf02174754e93a805c4793ab741b0919a32616e6
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2023-08-27 22:39:26 -1000

    Enable Netlify Form 📝

--- src/components/sections/ContactForm.svelte
+++ src/components/sections/ContactForm.svelte
@@ -1,20 +1,37 @@
-<form method="post" action="https://formsubmit.co/el/wobowe">
-  <label for="name"> Name </label>
-  <input type="text" id="name" name="name" required />
-  <label for="email"> Email </label>
-  <input type="email" id="email" name="email" required />
-  <label for="message"> Message </label>
-  <textarea id="message" name="message" required />
-  <button>Send</button>
+<form name="contact" method="post" netlify>
+  <div class="row">
+    <label id="namefield" for="name">
+      Name
+      <br />
+      <input type="text" id="name" name="name" required />
+    </label>
+    <label id="emailfield" for="email">
+      Email <br />
+      <input type="email" id="email" name="email" required />
+    </label>
+  </div>
+  <label id="messagefield" for="message">
+    Message
+    <br />
+    <textarea id="message" name="message" required />
+  </label>
+  <button id="button">Send</button>
 </form>
 
 <style>
   form {
-    display: grid;
-    border-radius: 8px;
     background: var(--color-bg-light);
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
     gap: 1rem;
     padding: 2rem;
+  }
+
+  .row {
+    display: flex;
+    gap: 1rem;
+    width: 100%;
   }
 
   :is(label, input, textarea, button) {
@@ -22,6 +39,7 @@
     background: inherit;
     border: none;
     color: var(--color-text-dark);
+    flex: 1;
     font-family: var(--font-family);
     outline: none;
   }
@@ -31,6 +49,7 @@
     border: 1px solid var(--color-text-dark);
     border-radius: 4px;
     padding: 0.5rem;
+    width: calc(100% - 1rem);
   }
 
   :is(input, textarea):hover,
@@ -39,11 +58,16 @@
     outline-offset: 0px;
   }
 
+  textarea {
+    height: 150px;
+  }
+
   button {
     background: var(--color-bg);
     border-radius: 4px;
     color: var(--color-text-light);
     padding: 1rem 2rem;
+    margin-inline: auto;
     width: fit-content;
   }
 

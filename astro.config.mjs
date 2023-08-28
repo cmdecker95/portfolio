7af8c6d6e6f4b25b95ed29d0120ced3b76d89547
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2023-08-28 02:21:01 -1000

    Remove Astro deps for Svelte and Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,1 @@
-import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
-
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [svelte()],
-  adapter: netlify(),
-});
+export default {};

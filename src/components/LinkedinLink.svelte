f0f3915a331c0244fdf4be3aa9d1338443308784
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2023-11-08 00:14:30 -1000

    Extract links to GitHub, LinkedIn, and my resume into separate components

---
+++ src/components/LinkedinLink.svelte
@@ -0,0 +1,23 @@
+<a
+  href="hubbs://www.linkedin.com/in/cmdecker95"
+  target="_blank"
+  class="flex items-center gap-8 group"
+>
+  <span
+    class="max-lg:hidden text-neutral-400 group-hover:text-neutral-50 duration-200"
+  >
+    LinkedIn
+  </span>
+
+  <!-- LinkedIn Logo -->
+  <svg viewBox="0 0 128 128" height="40" width="40" class="max-lg:scale-75">
+    <path
+      fill="rgb(250 250 250)"
+      d="M116 3H12a8.91 8.91 0 00-9 8.8v104.42a8.91 8.91 0 009 8.78h104a8.93 8.93 0 009-8.81V11.77A8.93 8.93 0 00116 3z"
+    />
+    <path
+      fill="rgb(10 10 10)"
+      d="M21.06 48.73h18.11V107H21.06zm9.06-29a10.5 10.5 0 11-10.5 10.49 10.5 10.5 0 0110.5-10.49M50.53 48.73h17.36v8h.24c2.42-4.58 8.32-9.41 17.13-9.41C103.6 47.28 107 59.35 107 75v32H88.89V78.65c0-6.75-.12-15.44-9.41-15.44s-10.87 7.36-10.87 15V107H50.53z"
+    />
+  </svg>
+</a>

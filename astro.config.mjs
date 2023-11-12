de10bb8f4ea349e80684b51788bcabe58884e33a
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2023-11-11 22:16:06 -1000

    Remove Svelte - unnecessary

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import svelte from "@astrojs/svelte";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind()],
   image: {
     service: passthroughImageService(),
   },

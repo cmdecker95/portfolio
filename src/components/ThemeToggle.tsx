e5670c50fd5072dae873f0280361164ba6c35e8c
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2025-04-17 00:27:44 -0400

    Bento redo (#4)
    * Crop photo
    
    * Fix Nav
    
    * Make ThemeToggle.tsx a bento component
    
    * Nuke blog
    
    * Inline the Font Awesome script
    
    * Cleanup home page elements
    
    * Redo Bento for max Bento power
    
    * Remove @tailwindcss/typography import

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -25,7 +25,7 @@ export default function ThemeToggle() {
   };
   return (
     <button
-      className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-zinc-200 hover:bg-zinc-300 dark:bg-zinc-800 dark:hover:bg-zinc-700"
+      className="flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-zinc-200 text-8xl hover:bg-zinc-300 dark:bg-zinc-800 dark:hover:bg-zinc-700"
       onClick={toggleTheme}
       aria-label={isDark ? "Switch to light mode" : "Switch to dark mode"}
     >
